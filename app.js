0c9b42ef921e0b08a4f4bbef7b98487e69fafd1a
Emily Ward <emilyrward15@gmail.com>
2024-02-09 17:56:51 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+require('express-async-errors');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+
+// USER AUTHENTICATION/AUTHORIZATION
+app.get()
