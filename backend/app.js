5225dfdfeeb035766365dac0af6a35967176f69c
Emily Ward <emilyrward15@gmail.com>
2024-02-12 22:04:11 -0700

    commiting

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
