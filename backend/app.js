d84a9e75a14db9d6cbf62ba480cb14bc82543588
Emily Ward <emilyrward15@gmail.com>
2024-02-12 22:23:44 -0700

    Initialize Express and Sequelize with CSRF protection

--- backend/app.js
+++ backend/app.js
@@ -8,3 +8,41 @@ const cookieParser = require('cookie-parser');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(cookieParser());
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+    helmet.crossOriginResourcePolicy({
+        policy: "cross-origin"
+    })
+);
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+);
+
+// backend/app.js
+const routes = require('./routes');
+
+app.use(routes); // Connect all the routes
+
+module.exports = app;
