d34c32c0db90210c958b6c0520dcc97253abde1a
Emily Ward <emilyrward15@gmail.com>
2024-02-20 20:34:49 -0700

    moving the route around in appjs

--- backend/app.js
+++ backend/app.js
@@ -20,64 +20,66 @@ app.use(express.json());
 // backend/app.js
 const routes = require('./routes');
 
-app.use(routes); // Connect all the routes
+
 
 // Security Middleware
 if (!isProduction) {
-    // enable cors only in development
-    app.use(cors());
+  // enable cors only in development
+  app.use(cors());
 }
 
 // helmet helps set a variety of headers to better secure your app
 app.use(
-    helmet.crossOriginResourcePolicy({
-        policy: "cross-origin"
-    })
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+  })
 );
 
 // Set the _csrf token and create req.csrfToken method
 app.use(
-    csurf({
-        cookie: {
-            secure: isProduction,
-            sameSite: isProduction && "Lax",
-            httpOnly: true
-        }
-    })
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
 );
 
+app.use(routes); // Connect all the routes
+
 app.use((_req, _res, next) => {
-    const err = new Error("The requested resource couldn't be found.");
-    err.title = "Resource Not Found";
-    err.errors = { message: "The requested resource couldn't be found." };
-    err.status = 404;
-    next(err);
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
 });
 
 // Process sequelize errors
 app.use((err, _req, _res, next) => {
-    // check if error is a Sequelize error:
-    if (err instanceof ValidationError) {
-      let errors = {};
-      for (let error of err.errors) {
-        errors[error.path] = error.message;
-      }
-      err.title = 'Validation error';
-      err.errors = errors;
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
     }
-    next(err);
-  });
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
 
-  app.use((err, _req, res, _next) => {
-    res.status(err.status || 500);
-    console.error(err);
-    res.json({
-      title: err.title || 'Server Error',
-      message: err.message,
-      errors: err.errors,
-      stack: isProduction ? null : err.stack
-    });
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
   });
+});
 
 
 
