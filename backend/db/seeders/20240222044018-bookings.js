db879fef98fd7ef3319ea76c5896f7dc5d3eb213
Emily Ward <emilyrward15@gmail.com>
2024-02-21 21:55:55 -0700

    Bookings seeder added

---
+++ backend/db/seeders/20240222044018-bookings.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+const { Booking } = require('../models');
+const bcrypt = require("bcryptjs");
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await Booking.bulkCreate([
+      {
+        spotId: 1,
+        userId: 1,
+        startDate: '01-01-2025',
+        endDate: '01-05-2025'
+      },
+      {
+        spotId: 2,
+        userId: 1,
+        startDate: '01-01-2025',
+        endDate: '01-05-2025'
+      },
+      {
+        spotId: 3,
+        userId: 2,
+        startDate: '01-01-2025',
+        endDate: '01-05-2025'
+      },
+      {
+        spotId: 1,
+        userId: 2,
+        startDate: '01-01-2025',
+        endDate: '01-05-2025'
+      },
+    ], { validate: true })
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'Bookings';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      id: { [Op.in]: [1, 2, 3, 4] }
+    }, {});
+  }
+};
