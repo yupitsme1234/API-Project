2f950478e84613ef23690fffd5998cc363a3c6a6
Emily Ward <emilyrward15@gmail.com>
2024-03-26 20:48:49 -0600

    Back at it again, updated authentication response

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -100,27 +100,5 @@ const validateSignup = [
     handleValidationErrors
 ];
 
-// // Sign up
-// router.post(
-//     '/',
-//     validateSignup,
-//     async (req, res) => {
-//         const { email, password, username } = req.body;
-//         const hashedPassword = bcrypt.hashSync(password);
-//         const user = await User.create({ email, username, hashedPassword });
-
-//         const safeUser = {
-//             id: user.id,
-//             email: user.email,
-//             username: user.username,
-//         };
-
-//         await setTokenCookie(res, safeUser);
-
-//         return res.json({
-//             user: safeUser
-//         });
-//     }
-// );
 
 module.exports = router;
