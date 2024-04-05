5e4e497be5c712021ebef97decc92e83034ae4b3
Emily Ward <emilyrward15@gmail.com>
2024-04-04 21:58:31 -0600

    ugh

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -22,7 +22,7 @@ router.get('/current', requireAuth, async (req, res, next) => {
     // }
 
 
-    const user = await User.one({
+    const user = await User.findOne({
         where: {
             id: userId
         },
